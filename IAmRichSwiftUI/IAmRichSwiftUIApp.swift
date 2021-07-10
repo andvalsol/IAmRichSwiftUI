114b8c070fac2931d962973329b2b3f6a29b954e
Andrey Solera <andrey.valverde.solera@gmail.com>
2021-07-10 07:58:43 -0600

    Initial Commit

---
+++ IAmRichSwiftUI/IAmRichSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IAmRichSwiftUIApp.swift
+//  IAmRichSwiftUI
+//
+//  Created by Andrey Solera on 10/7/21.
+//
+
+import SwiftUI
+
+@main
+struct IAmRichSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
